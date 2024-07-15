138e3106f3dcb6bf3555d511818b7f59d8a47872
James Luncan <jluncan@tql.com>
2024-07-15 15:33:32 -0400

    initial commit

---
+++ src/app/prs/User/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user.class';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseurl: string = "http://localhost:5000/api/users";
+
+  constructor( private http: HttpClient ) { }
+
+  list(): Observable<User[]> {
+    return this.http.get(`${this.baseurl}`) as Observable<User[]>;
+  }
+}
